8502e1381f774b0dc2adaf94bc1f3dd2cb60cd8c
Larry Kagan <larry@superiocity.com>
2020-06-15 10:39:55 -0400

    Add a banner when there's no current tasks

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -2,7 +2,8 @@
   <q-page class="flex">
 
     <div class="q-pa-md full-width">
-      <tasks-todo :tasks="tasksTodo" v-if="Object.keys(tasksTodo).length"></tasks-todo>
+      <NoTasks v-if="Object.keys(tasksTodo).length === 0" @showAddTask="showAddTaskForm = true"></NoTasks>
+      <tasks-todo :tasks="tasksTodo" v-else></tasks-todo>
       <tasks-completd :tasks="tasksCompleted" v-if="Object.keys(tasksCompleted).length"></tasks-completd>
       <div class="absolute-bottom text-center q-mb-md">
         <q-btn size="24"
@@ -26,11 +27,12 @@
   import AddTask from "components/AddTask";
   import TasksTodo from "components/TasksTodo";
   import TasksCompletd from "components/TasksCompleted";
+  import NoTasks from "components/NoTasks";
 
   export default {
     name: 'Tasks',
 
-    components: {TasksCompletd, TasksTodo, AddTask, Task},
+    components: {NoTasks, TasksCompletd, TasksTodo, AddTask, Task},
 
     data() {
       return {
