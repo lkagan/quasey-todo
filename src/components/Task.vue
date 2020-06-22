989b9bb994c85b112ac01ad809ad56b62428e482
Larry Kagan <larry@superiocity.com>
2020-06-22 15:15:57 -0400

    Refactor tabs to 4 spaces

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,94 +1,95 @@
 <template>
-  <q-item
-    clickable
-    v-ripple
-    @click="updateTask({id, updates: {complete: !task.complete}})"
-    :class="task.complete ? 'text-grey-5' : ''"
-    v-touch-hold:1000.mouse="() => {showEditTask = true}"
-  >
-    <q-item-section side top>
-      <q-checkbox v-model="task.complete" class="no-pointer-events"/>
-    </q-item-section>
-
-    <q-item-section>
-      <q-item-label>{{ task.name }}</q-item-label>
-    </q-item-section>
-
-    <q-item-section side top v-if="task.date">
-      <q-item-label
-        caption
+    <q-item
+        clickable
+        v-ripple
+        @click="updateTask({id, updates: {complete: !task.complete}})"
         :class="task.complete ? 'text-grey-5' : ''"
-      >
-        <q-icon name="event"/>
-        {{ task.date | niceDate }}
-      </q-item-label>
-    </q-item-section>
+        v-touch-hold:1000.mouse="() => {showEditTask = true}"
+    >
+        <q-item-section side top>
+            <q-checkbox v-model="task.complete" class="no-pointer-events"/>
+        </q-item-section>
 
-    <q-item-section side>
-      <q-btn flat round dense icon="edit"
-             @click.stop="showEditTask = true"/>
-    </q-item-section>
+        <q-item-section>
+            <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
 
-    <q-item-section side>
-      <q-btn flat round dense icon="delete"
-             @click.stop="promptToDelete(id)"/>
-    </q-item-section>
+        <q-item-section side top v-if="task.date">
+            <q-item-label
+                caption
+                :class="task.complete ? 'text-grey-5' : ''"
+            >
+                <q-icon name="event"/>
+                {{ task.date | niceDate }}
+            </q-item-label>
+        </q-item-section>
 
-    <q-dialog v-model="showEditTask">
-      <EditTask :task="task" :id="id" @close="showEditTask = false"></EditTask>
-    </q-dialog>
+        <q-item-section side>
+            <q-btn flat round dense icon="edit"
+                   @click.stop="showEditTask = true"/>
+        </q-item-section>
 
-  </q-item>
+        <q-item-section side>
+            <q-btn flat round dense icon="delete"
+                   @click.stop="promptToDelete(id)"/>
+        </q-item-section>
+
+        <q-dialog v-model="showEditTask">
+            <EditTask :task="task" :id="id"
+                      @close="showEditTask = false"></EditTask>
+        </q-dialog>
+
+    </q-item>
 </template>
 
 <script>
-  import {mapMutations} from 'vuex';
-  import {deleteTask} from "src/store/module-tasks/mutations";
-  import EditTask from "components/EditTask";
-  import {date} from 'quasar';
+    import {mapMutations} from 'vuex';
+    import {deleteTask} from "src/store/module-tasks/mutations";
+    import EditTask from "components/EditTask";
+    import {date} from 'quasar';
 
-  export default {
-    name: "Task",
-    components: {EditTask},
-    props: {
-      'task': {
-        required: true,
-        type: Object,
-      },
-      'id': {
-        required: true,
-        type: String,
-      }
-    },
+    export default {
+        name: "Task",
+        components: {EditTask},
+        props: {
+            'task': {
+                required: true,
+                type: Object,
+            },
+            'id': {
+                required: true,
+                type: String,
+            }
+        },
 
-    data() {
-      return {
-        showEditTask: false,
-      }
-    },
+        data() {
+            return {
+                showEditTask: false,
+            }
+        },
 
-    methods: {
-      ...mapMutations('tasks', ['updateTask', 'deleteTask']),
+        methods: {
+            ...mapMutations('tasks', ['updateTask', 'deleteTask']),
 
-      promptToDelete(id) {
-        this.$q.dialog({
-          dark: true,
-          title: 'Confirm',
-          message: 'Are you sure you want to delete this task?',
-          cancel: true,
-          persistent: true
-        }).onOk(() => {
-          this.deleteTask(id)
-        });
-      }
-    },
+            promptToDelete(id) {
+                this.$q.dialog({
+                    dark: true,
+                    title: 'Confirm',
+                    message: 'Are you sure you want to delete this task?',
+                    cancel: true,
+                    persistent: true
+                }).onOk(() => {
+                    this.deleteTask(id)
+                });
+            }
+        },
 
-    filters: {
-      niceDate(value) {
-        return date.formatDate(value, 'MMM D');
-      }
+        filters: {
+            niceDate(value) {
+                return date.formatDate(value, 'MMM D');
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
